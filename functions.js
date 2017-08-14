b95d8b13f5ea440543d4c2cc3f8a23cfc01db71e
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-08-14 12:10:06 -1000

    finished isOdd function

--- functions.js
+++ functions.js
@@ -216,6 +216,13 @@ function isEven(n){
  * @return {boolean} the number is odd
  */
 
+function isOdd(n){
+	if(n % 2 !== 0){
+		return true;
+	}else{
+		return false;
+	}
+}
 
 /**
  * Returns a letter grade.
