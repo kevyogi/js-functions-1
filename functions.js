a40addabd55eb4b03dd2fd0ca4334ac37b019266
Kevin Yogi <kevinyogi@Kevins-MacBook-Pro.local>
2017-08-14 12:08:44 -1000

    finished isEven funciton

--- functions.js
+++ functions.js
@@ -202,6 +202,13 @@ function maximum(x, y){
  * @return {boolean} the number is even
  */
 
+function isEven(n){
+	if(n % 2 === 0){
+		return true;
+	}else{
+		return false;
+	}
+}
 
 /**
  * Returns true if `n` is odd.
